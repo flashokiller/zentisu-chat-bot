e8d1c11de1d7e16673c66dd0b8fe0bab3dc57649
flashokiller <flashokiller@gmail.com>
2021-07-21 19:11:23 +0530

    meme

---
+++ config.py
@@ -0,0 +1,10 @@
+bot_token = "botm token" # yourbot token
+ARQ_API_KEY = "ARQTOMKEN" # Get this from @ARQRoBot
+api_id = 69 # your api id
+api_hash = "M2EOW" #5 your apihash
+# List of supported languages >>
+# https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
+# Leave it as it is
+LANGUAGE = "en"
+# Leave it as it is
+ARQ_API_BASE_URL = "https://thearq.tech"
